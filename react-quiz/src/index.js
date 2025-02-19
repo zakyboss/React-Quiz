1f672941e2a689e03ab24e5212674e08f6076f63
Zakyboss <Boochimo8@gmail.com>
2025-02-19 03:30:17 +0300

    Project complete

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Component/App";
+import { QuizContextCreator } from "./Component/QuizContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <QuizContextCreator>
     <App />
+    </QuizContextCreator>
   </React.StrictMode>
 );
 
